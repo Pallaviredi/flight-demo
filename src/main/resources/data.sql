7ecef5bdc7309772817e76519684422d7838b89c
PallaviReddy25 <pallavibobbala19@gmail.com>
2026-04-26 20:13:56 -0500

    data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO flight (origin, destination, flight_number, price, departure_time) VALUES
+('DFW','ORD',101,45,'8:00 AM'),
+('DFW','ORD',102,55,'9:00 AM'),
+('DFW','ORD',103,65,'6:00 PM'),
+('DFW','LIT',201,34,'2:00 PM'),
+('LIT','JFK',301,65,'5:00 PM');
